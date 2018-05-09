e535233c1e78ea54a5aa088e077bb5e35b9daf5d
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2018-05-09 18:59:39 -0400

    Added executable to fixup duplicate/extraneous messages
    It will remove extraneous PLAYER_NEW and PLAYER_LEVEL
    messages and add missing PLAYER_LEAVE messages when
    statsbot rejoins. This fixes some difficulties in
    parsing logs which span multiple games.

---
+++ player-new-cleanse/src/main.rs
@@ -0,0 +1,113 @@
+
+extern crate statslib;
+
+use statslib::*;
+
+use std::io::{BufReader, BufRead, stdin};
+use std::collections::HashMap;
+use std::mem;
+use std::fs::File;
+use std::env::args;
+
+fn main() {
+	let args: Vec<String> = args().collect();
+	let file = File::open(args[1].clone()).unwrap();
+
+    let mut players: HashMap<i64, ()> = HashMap::new();
+    let mut is_login = false;
+    let mut newplayers: HashMap<i64, ()> = HashMap::new();
+    let mut msgbuffer: Vec<String> = vec![];
+
+	for line in BufReader::new(file).lines() {
+		let ln = line.unwrap();
+		let result = parse(&ln);
+
+		if result.is_err() {
+			    eprintln!("Invalid Record: {}", ln);
+			continue;
+		}
+		let record = result.unwrap();
+
+        if is_login && !(record.tag == "PLAYER_NEW" || record.tag == "PLAYER_LEVEL") {
+            {
+                 let mut leftplayers = players.clone();
+
+                for (player, &()) in newplayers.iter() {
+                    leftplayers.remove(player);
+                }
+
+                let mut it1 = leftplayers.iter().map(|(player, _)| {
+                    format!("[PLAYER_LEAVE,id:{}]", *player).to_string()
+                });
+
+                let tmpbuf = mem::replace(&mut msgbuffer, vec![]);
+
+                let it2 = tmpbuf.into_iter().filter(|x| {
+                    let record = parse(&x).unwrap();
+
+                    if record.tag == "PLAYER_NEW" || record.tag == "PLAYER_LEVEL" {
+                        let id = match record.entries["id"] {
+                            RecordValue::Int(val) => val,
+                            _ => panic!()
+                        };
+
+                        let result = !players.contains_key(&id);
+
+                        result
+                    }
+                    else {
+                        true
+                    }
+                });
+
+                
+                for msg in it1 {
+                    println!("{}", msg);
+                }
+                for msg in it2 {
+                    println!("{}", msg);
+                }
+            }
+            players = mem::replace(&mut newplayers, HashMap::new());
+            is_login = false;
+        }
+
+		if record.tag == "LOGIN" {
+            is_login = true;
+        }
+        else if record.tag == "PLAYER_NEW" {
+            let id = match record.entries["id"] {
+                RecordValue::Int(val) => val,
+                _ => {
+                    eprintln!("Invalid record: {}", ln);
+                    continue;
+                }
+            };
+
+            if is_login {
+                newplayers.insert(id, ());
+            }
+            else {
+                players.insert(id, ());
+            }
+        }
+        else if record.tag == "PLAYER_LEAVE" {
+            let id = match record.entries["id"] {
+                RecordValue::Int(val) => val,
+                _ => {
+                    eprintln!("Invalid record: {}", ln);
+                    continue;
+                }
+            };
+
+            players.remove(&id);
+        }
+
+        if !is_login {
+            println!("{}", ln);
+        }
+        else {
+            msgbuffer.push(ln.to_string());
+        }
+	}
+}
